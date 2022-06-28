6ac2e06fbf43e1d52c67f9ae51199b651e284774
Brett Graves <brett.graves@xakiatech.com>
2022-06-28 15:14:28 +1000

    custom fields

---
+++ src/Xakia.API.Client/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Xakia.API.Client")]
+[assembly: AssemblyDescription("Dotnet core client library for the Xakia API")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Xakia Technologies")]
+[assembly: AssemblyProduct("Xakia.API.Client")]
+[assembly: AssemblyCopyright("Copyright © Xakia 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4605a381-fbcc-4a66-b12c-664e35aa4928")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.*")]
