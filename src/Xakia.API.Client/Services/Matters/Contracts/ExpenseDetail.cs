c03e9c00db82562495b36643f5c3d7b9524ac10c
Brett Graves <brett.graves@xakiatech.com>
2022-07-26 16:55:38 +1000

    update properties on a matter

---
+++ src/Xakia.API.Client/Services/Matters/Contracts/ExpenseDetail.cs
@@ -0,0 +1,23 @@
+﻿using NodaTime;
+using System;
+
+namespace Xakia.API.Client.Services.Matters.Contracts
+{
+    public class ExpenseDetail
+    {
+        public Guid ExpenseId { get; set; }
+
+        public LocalDate InvoiceDate { get; set; }
+
+        public string InvoiceNumber { get; set; }
+
+        public string Currency { get; set; }
+
+        public double ExchangeRate { get; set; }
+
+        public decimal Total { get; set; }
+
+        public string Notes { get; set; }
+
+    }
+}
---
+++ src/Xakia.API.Client/Services/Matters/Contracts/UpdateMatterRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xakia.API.Client.Services.Matters.Contracts
+{
+    public class UpdateMatterRequest : IContract
+    {
+        public OptionalMatterProperties MatterProperties { get; set; }
+    }
+}
